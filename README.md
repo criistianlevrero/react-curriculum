8377a06baaba2d8ea459b8ca19409da763aa1051
criistianlevrero <cristianlevrero@gmail.com>
2022-03-16 19:11:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fgmrmc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fgmrmc)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+
+  dataModel: {
+    profileImage:''
+  }
+  return (
+    <main>
+      <aside>
+        <img alt="una bella foto"/>
+        <contactCard iconList=""></contactCard>
+      </aside>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </main>
+  );
+}
+
