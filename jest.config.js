dd02199e784500a34959642ff9efce6cbb28e1a3
Cristian Levrero <cristianlevrero@gmail.com>
2023-10-11 17:30:10 -0300

    feat: style modules jest fix

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,9 @@ export default {
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/src/__mocks__/fileMock.cjs',
-        '\\.(css|less)$': 'identity-obj-proxy',
+        '\\.(scss|less)$': 'identity-obj-proxy',
     },
+    testEnvironment: "jsdom",
     // important to expose all the jest-dom api globally in each test
     setupFilesAfterEnv: ["@testing-library/jest-dom/"]
 };
