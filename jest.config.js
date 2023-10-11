52d75d511601618791ab1befd92d9aef90d61e31
Cristian Levrero <cristianlevrero@gmail.com>
2023-10-11 15:02:41 -0300

    feat: jest and babel basic configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest'
+    },
+    testMatch: ['**/*.test.jsx'],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/src/__mocks__/fileMock.cjs',
+        '\\.(css|less)$': 'identity-obj-proxy',
+    },
+    // important to expose all the jest-dom api globally in each test
+    setupFilesAfterEnv: ["@testing-library/jest-dom/"]
+};
---
+++ src/components/button/button.test.jsx
@@ -0,0 +1,21 @@
+import { test, expect } from '@jest/globals';
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Button } from './button';
+
+test('renders button with icon', () => {
+  render(<Button icon="search">Search</Button>);
+  const buttonElement = screen.getByRole('button');
+  const iconElement = screen.getByTestId('icon');
+  expect(buttonElement).toBeInTheDocument();
+  expect(iconElement).toBeInTheDocument();
+});
+
+test('renders button without icon', () => {
+  render(<Button>Submit</Button>);
+  const buttonElement = screen.getByRole('button');
+  const iconElement = screen.queryByTestId('icon');
+  expect(buttonElement).toBeInTheDocument();
+  expect(iconElement).toBeNull();
+});
